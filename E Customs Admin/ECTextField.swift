0135a13511b648d79cc4e9403545184022acfc98
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-11 07:31:37 +0530

    Required files added from E Customs app

---
+++ E Customs Admin/ECTextField.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+class ECTextField: UITextField {
+    
+    // MARK: Properties
+    fileprivate var padding: CGFloat = 0
+    
+    
+    // MARK: Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    convenience init(background: UIColor = .white, padding: CGFloat, placeholderText: String = "", radius: CGFloat = 0) {
+        self.init()
+        self.padding = padding
+        self.placeholder = placeholderText
+        self.layer.cornerRadius = radius
+    }
+    
+    
+    required init?(coder: NSCoder) { fatalError() }
+
+    
+    // MARK: Overridden Methods
+    override func editingRect(forBounds bounds: CGRect) -> CGRect { return bounds.insetBy(dx: padding, dy: 0) }
+    
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect { return bounds.insetBy(dx: padding, dy: 0) }
+    
+    
+    override var intrinsicContentSize: CGSize { return .init(width: 0, height: 50) }
+}

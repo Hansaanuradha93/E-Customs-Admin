2c7b8d0abcfd7667ed5aa101dbc4e653f8037460
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-12 16:12:38 +0530

    Requred view controllers and view models added

---
+++ E Customs Admin/HomeVC.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationController?.navigationBar.isHidden = true
+    }
+}

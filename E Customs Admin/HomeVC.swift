d07b4e2806aedb2770ab3375aba607e256a1b199
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-14 22:23:02 +0530

    HomeVC refactored

--- E Customs Admin/HomeVC.swift
+++ E Customs Admin/HomeVC.swift
@@ -3,27 +3,40 @@ import Firebase
 
 class HomeVC: UIViewController {
 
+    // MARK: Properties
     let viewModel = HomeVM()
-    
     fileprivate var listener: ListenerRegistration?
     
     
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        navigationController?.navigationBar.isHidden = true
-        
-        listener = viewModel.fetchProducts { (status) in
-            if status {
-                print(self.viewModel.products)
-                print("count", self.viewModel.products.count)
-            }
-        }
+        setupUI()
+        fetchProducts()
     }
     
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         if isMovingFromParent { listener?.remove() }
+    }
+}
+
+
+// MARK: - Methods
+extension HomeVC {
+    
+    fileprivate func fetchProducts() {
+        listener = viewModel.fetchProducts { (status) in
+            if status {
+                print(self.viewModel.products)
+            }
+        }
+    }
+    
+    
+    fileprivate func setupUI() {
+        view.backgroundColor = .white
+        navigationController?.navigationBar.isHidden = true
     }
 }

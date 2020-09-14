4e716c094c464f9bb86dd465c1166a352270c21f
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-14 22:20:11 +0530

    fetch profucts integrated to HomeVX

--- E Customs Admin/HomeVC.swift
+++ E Customs Admin/HomeVC.swift
@@ -1,10 +1,29 @@
 import UIKit
+import Firebase
 
 class HomeVC: UIViewController {
 
+    let viewModel = HomeVM()
+    
+    fileprivate var listener: ListenerRegistration?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         navigationController?.navigationBar.isHidden = true
+        
+        listener = viewModel.fetchProducts { (status) in
+            if status {
+                print(self.viewModel.products)
+                print("count", self.viewModel.products.count)
+            }
+        }
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if isMovingFromParent { listener?.remove() }
     }
 }

65a05cfe89a5bab2e153008323cf44f340dab163
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-14 22:19:17 +0530

    fetch products from firestore added to HomeVM

--- E Customs Admin/HomeVM.swift
+++ E Customs Admin/HomeVM.swift
@@ -1,3 +1,36 @@
 import UIKit
+import Firebase
 
-class HomeVM {}
+class HomeVM {
+    
+    // MARK: Properties
+    var products = [Product]()
+}
+
+
+// MARK: - Methods
+extension HomeVM {
+    
+    func fetchProducts(completion: @escaping (Bool) -> ()) -> ListenerRegistration? {
+        let reference = Firestore.firestore().collection("products")
+        let listener = reference.addSnapshotListener { querySnapshot, error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(false)
+                return
+            }
+            guard let documentChanges = querySnapshot?.documentChanges else {
+                completion(false)
+                return
+            }
+            for change in documentChanges {
+                if change.type == .added {
+                    let product = Product(dictionary: change.document.data())
+                    self.products.append(product)
+                }
+            }
+            completion(true)
+        }
+        return listener
+    }
+}

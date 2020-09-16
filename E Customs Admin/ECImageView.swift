53922d6732423ad555feced895e89c200ad36307
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-16 22:47:27 +0530

    ECImageView added

---
+++ E Customs Admin/ECImageView.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class ECImageView: UIImageView {
+    
+    // MARK: Initialisers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) { fatalError() }
+
+    
+    convenience init(image: UIImage, borderWidth: CGFloat = 0, borderColor: UIColor = .clear, contentMode: ContentMode = .scaleAspectFill) {
+        self.init(frame: .zero)
+        self.image = image
+        self.layer.borderWidth = borderWidth
+        self.layer.borderColor = borderColor.cgColor
+        self.contentMode = contentMode
+        self.clipsToBounds = true
+    }
+}

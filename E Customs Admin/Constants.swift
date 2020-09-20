a0d23f1a6d81f3bb5366bd793b9a57fb487072d6
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-20 22:29:29 +0530

    system images added to asserts

--- E Customs Admin/Constants.swift
+++ E Customs Admin/Constants.swift
@@ -2,4 +2,12 @@ import UIKit
 
 struct Asserts {
     static let placeHolder = UIImage(named: "placeholder")!
+    
+    static let house = UIImage(systemName: "house")
+    static let houseFill = UIImage(systemName: "house.fill")
+    static let plus = UIImage(systemName: "plus")
+    static let document = UIImage(systemName: "doc.text")
+    static let documentFill = UIImage(systemName: "doc.text.fill")
+    static let envelope = UIImage(systemName: "envelope")
+    static let envelopeFill = UIImage(systemName: "envelope.fill")
 }

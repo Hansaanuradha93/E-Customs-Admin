7bd59176666fba0908637632ce1be4ec4cacaf36
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-21 22:06:12 +0530

    ECTabBar strings refactored

--- E Customs Admin/ECTabBar.swift
+++ E Customs Admin/ECTabBar.swift
@@ -16,28 +16,28 @@ extension ECTabBar {
     
     fileprivate func createHomeNC() -> UINavigationController {
         let homeVC = HomeVC()
-        homeVC.tabBarItem = UITabBarItem(title: "", image: Asserts.house, selectedImage: Asserts.houseFill)
+        homeVC.tabBarItem = UITabBarItem(title: Strings.empty, image: Asserts.house, selectedImage: Asserts.houseFill)
         return UINavigationController(rootViewController: homeVC)
     }
     
     
     fileprivate func createAddProductNC() -> UINavigationController {
         let addProductVC = AddProductsVC()
-        addProductVC.tabBarItem = UITabBarItem(title: "", image: Asserts.plus, selectedImage: Asserts.plus)
+        addProductVC.tabBarItem = UITabBarItem(title: Strings.empty, image: Asserts.plus, selectedImage: Asserts.plus)
         return UINavigationController(rootViewController: addProductVC)
     }
     
     
     fileprivate func createOrderListNC() -> UINavigationController {
         let orderListVC = OrderListVC()
-        orderListVC.tabBarItem = UITabBarItem(title: "", image: Asserts.document, selectedImage: Asserts.documentFill)
+        orderListVC.tabBarItem = UITabBarItem(title: Strings.empty, image: Asserts.document, selectedImage: Asserts.documentFill)
         return UINavigationController(rootViewController: orderListVC)
     }
     
     
     fileprivate func createRequestBoxNC() -> UINavigationController {
         let requestBoxVC = RequestBoxVC()
-        requestBoxVC.tabBarItem = UITabBarItem(title: "", image: Asserts.envelope, selectedImage: Asserts.envelopeFill)
+        requestBoxVC.tabBarItem = UITabBarItem(title: Strings.empty, image: Asserts.envelope, selectedImage: Asserts.envelopeFill)
         return UINavigationController(rootViewController: requestBoxVC)
     }
 }

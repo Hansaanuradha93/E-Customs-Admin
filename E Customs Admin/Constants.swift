a1b79bc1dd846bf1a2170a21c3d99806fd294191
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-09-21 22:05:53 +0530

    String constants and image constants added

--- E Customs Admin/Constants.swift
+++ E Customs Admin/Constants.swift
@@ -1,8 +1,11 @@
 import UIKit
 
 struct Asserts {
+    
+    // Images
     static let placeHolder = UIImage(named: "placeholder")!
     
+    // System Icons
     static let house = UIImage(systemName: "house")
     static let houseFill = UIImage(systemName: "house.fill")
     static let plus = UIImage(systemName: "plus")
@@ -10,4 +13,25 @@ struct Asserts {
     static let documentFill = UIImage(systemName: "doc.text.fill")
     static let envelope = UIImage(systemName: "envelope")
     static let envelopeFill = UIImage(systemName: "envelope.fill")
+}
+
+
+struct Strings {
+    
+    // Placeholders
+    static let empty = ""
+    static let enterEmail = "Enter email"
+    static let enterPassword = "Enter password"
+    static let enterFullName = "Enter full name"
+    
+    // Alerts
+    static let loginFailed = "Login Failed!"
+    static let signupFailed = "Signup Failed!"
+    
+    // Buttons
+    static let ok = "OK"
+    static let login = "Log In"
+    static let gotoSignup = "Go to sign up"
+    static let signup = "Sign Up"
+    static let gotoLogin = "Go to login"
 }

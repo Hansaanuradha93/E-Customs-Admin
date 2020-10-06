6bc99400e34a1fa241613a7c856d06492c567c59
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-10-06 21:26:57 +0530

    Requests fetched from firestore and populated on Requests screen

--- E Customs Admin/RequestBoxVC.swift
+++ E Customs Admin/RequestBoxVC.swift
@@ -1,9 +1,67 @@
 import UIKit
+import Firebase
 
-class RequestBoxVC: UIViewController {
+class RequestBoxVC: UITableViewController {
+    
+    let viewModel = RequestBoxVM()
+    fileprivate var listener: ListenerRegistration?
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        setupTableView()
+        fetchRequests()
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if isMovingFromParent { listener?.remove() }
+    }
+    
+    
+    fileprivate func fetchRequests() {
+        listener = viewModel.fetchRequest { [weak self] status in
+            guard let self = self else { return }
+            if status {
+                DispatchQueue.main.async { self.tableView.reloadData() }
+            }
+        }
+    }
+    
+    
+    fileprivate func setupTableView() {
+        tableView.separatorStyle = .none
+        tableView.register(RequestCell.self, forCellReuseIdentifier: RequestCell.reuseID)
+    }
+    
+    
+    fileprivate func setupUI() {
+        navigationController?.navigationBar.barTintColor = UIColor.white
         view.backgroundColor = .white
+        title = Strings.requestBox
+        tabBarItem.title = Strings.empty
+    }
+}
+
+
+// MARK: - TableView
+extension RequestBoxVC {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.requests.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: RequestCell.reuseID, for: indexPath) as! RequestCell
+        cell.set(request: viewModel.requests[indexPath.row])
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 170
     }
 }
--- E Customs Admin/RequestBoxVM.swift
+++ E Customs Admin/RequestBoxVM.swift
@@ -1,3 +1,39 @@
 import UIKit
+import Firebase
 
-class RequestBoxVM {}
+class RequestBoxVM {
+    
+    // MARK: Properties
+    var requests = [Request]()
+}
+
+
+// MARK: - Methods
+extension RequestBoxVM {
+    
+    func fetchRequest(completion: @escaping (Bool) -> ()) -> ListenerRegistration? {
+        let reference = Firestore.firestore().collection("requests")
+        
+        requests.removeAll()
+        
+        let listener = reference.addSnapshotListener { querySnapshot, error in
+            if let error = error {
+                print(error)
+                completion(false)
+                return
+            }
+            guard let documentChanges = querySnapshot?.documentChanges else {
+                completion(false)
+                return
+            }
+            for change in documentChanges {
+                if change.type == .added {
+                    let request = Request(dictionary: change.document.data())
+                    self.requests.append(request)
+                }
+            }
+            completion(true)
+        }
+        return listener
+    }
+}

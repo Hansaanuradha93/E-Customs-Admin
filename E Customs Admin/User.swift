c90acdb8ffc2d11e1177fb03766ad54bfbec3e2d
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-10-08 17:19:15 +0530

    User Model added

---
+++ E Customs Admin/User.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+struct User {
+    
+    // MARK: Properties
+    var uid, firstname, lastname, email: String?
+    var isAdminUser, isMale: Bool?
+    
+    
+    // MARK: Initializers
+    init(dictionary: [String : Any]) {
+        self.uid = dictionary["uid"] as? String
+        self.firstname = dictionary["firstname"] as? String
+        self.lastname = dictionary["lastname"] as? String
+        self.lastname = dictionary["lastname"] as? String
+        self.email = dictionary["email"] as? String
+        self.isAdminUser = dictionary["isAdminUser"] as? Bool
+        self.isMale = dictionary["isMale"] as? Bool
+    }
+}

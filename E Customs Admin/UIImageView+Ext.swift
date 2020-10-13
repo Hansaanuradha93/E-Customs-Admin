80bd55d0dcee72cca2e084a7cdfe2925b462f425
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-10-13 22:23:26 +0530

    image caching bug fixed

--- E Customs Admin/UIImageView+Ext.swift
+++ E Customs Admin/UIImageView+Ext.swift
@@ -3,6 +3,7 @@ import UIKit
 extension UIImageView {
     
     func downloadImage(from url: String) {
+        self.image = Asserts.placeHolder
         NetworkManager.shared.downloadImage(from: url) { [weak self] image in
             guard let self = self else { return }
             DispatchQueue.main.async { self.image = image }
